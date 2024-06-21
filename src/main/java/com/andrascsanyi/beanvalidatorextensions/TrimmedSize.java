6f1aa7c2945f4f7fcedbdabe0a233af2c7845dc7
Andras Csanyi <andras.csanyi@pm.me>
2024-06-21 20:24:05 +0200

    reshape3

--- src/main/java/com/andrascsanyi/beanvalidatorextensions/TrimmedSize.java
+++ src/main/java/com/andrascsanyi/beanvalidatorextensions/TrimmedSize.java
@@ -9,6 +9,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ *
+ */
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = TrimmedSizeValidator.class)
