a8abc90e899dbc25d5d976d0028dfd64cb6b3492
Andras Csanyi <andras.csanyi@pm.me>
2024-06-21 20:39:48 +0200

    doc
    Signed-off-by: Andras Csanyi <andras.csanyi@pm.me>

--- src/main/java/com/andrascsanyi/beanvalidatorextensions/TrimmedSize.java
+++ src/main/java/com/andrascsanyi/beanvalidatorextensions/TrimmedSize.java
@@ -10,7 +10,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- *
+ * Add documentation here
  */
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
